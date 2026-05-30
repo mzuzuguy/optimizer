8b215621783523406c424506879959118e65de83
mzuzuguy <preciousmusole18@gmail.com>
2026-05-30 14:28:04 +0200

    defined queueing

---
+++ packet.h
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+using namespace std;
+
+struct packet
+{
+        string packetId;
+        long long timestamp;
+        string serviceType;
+        int priority;
+        string payload;
+    
+        
+};
